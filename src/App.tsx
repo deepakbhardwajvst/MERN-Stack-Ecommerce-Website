54e9bdec459ea56b94ada5af7dd7b17dd99fb26c
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-17 00:48:01 +0530

    Integrate lazy loading with Suspense for route-based code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "./App.css"
+import { lazy, Suspense } from "react";
+import Loader from "./Components/loader";
+const Home = lazy(() => import("./pages/home"))
+const Search = lazy(() => import('./pages/search'))
+const Cart = lazy(() => import("./pages/cart"))
+
 
 function App() {
 
   return (
-   <></>
+    <Router>
+      {/* Header */}
+      <Suspense fallback={<Loader/>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
+    </Router>
   )
 }
-
 export default App
