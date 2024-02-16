3b2b11a69a4f53226b153a552c2b1b109a57b4ef
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-17 01:21:56 +0530

    Lazy load React components using React.lazy() for code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,24 @@ import Loader from "./Components/loader";
 const Home = lazy(() => import("./pages/home"))
 const Search = lazy(() => import('./pages/search'))
 const Cart = lazy(() => import("./pages/cart"))
-
-
+// importing admin route
+const Dashboard = lazy(() => import("./pages/admin/dashboard"));
+const Products = lazy(() => import("./pages/admin/products"));
+const Customers = lazy(() => import("./pages/admin/customers"));
+const Transaction = lazy(() => import("./pages/admin/transaction"));
+const Barcharts = lazy(() => import("./pages/admin/charts/barcharts"));
+const Piecharts = lazy(() => import("./pages/admin/charts/piecharts"));
+const Linecharts = lazy(() => import("./pages/admin/charts/linecharts"));
+const Coupon = lazy(() => import("./pages/admin/apps/coupon"));
+const Stopwatch = lazy(() => import("./pages/admin/apps/stopwatch"));
+const Toss = lazy(() => import("./pages/admin/apps/toss"));
+const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
+const ProductManagement = lazy(
+  () => import("./pages/admin/management/productmanagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/admin/management/transactionmanagement")
+);
 function App() {
 
   return (
