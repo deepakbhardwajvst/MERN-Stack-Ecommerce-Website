44fe0368e43f1666faa83d7416a1f7a4af36541b
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-29 16:24:39 +0530

    Implemented the 'Home' component with the following features: - Utilized the 'react-router-dom' library for linking to '/search'. - Integrated the 'ProductCard' component to display the latest product information. - Defined a placeholder 'addToCartHandler' function for handling product additions to the cart. - Styled the 'Latest Products' section with a link to '/search'. - Enhanced the component with relevant details, including product ID, name, price, stock, and a product photo.

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -12,8 +12,8 @@ type ProductsProps = {
 const server = "fhdg"
 const ProductCard = ({ productId, photo, name, price, stock, handler }: ProductsProps) => {
   return (
-    <div className="productCard">
-      <img src={`${server}/${photo}`} alt={name} className="" />
+    <div className="product-card">
+      <img src={`${photo}`} alt={name} className="" />
       <p className="name">{name}</p>
       <span className="price">₹{price}</span>
       <div className=""><button onClick={() => handler()}><FaPlus /></button></div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,18 @@
 
 import { Link } from 'react-router-dom';
+import ProductCard from '../Components/ProductCard.tsx';
 
 const Home = () => {
+  const addToCartHandler = () => {
+
+  }
   return (
     <div className="home">
       <section className=""></section>
       <h1 className="">Latest Products
         <Link to="/search" className='findmore'>More</Link></h1>
-      <main className=""></main>
+      <main className=""><ProductCard productId='dfs' name='mac' handler={addToCartHandler} price={4545} stock={435} photo='https://m.media-amazon.com/images/I/316ArzLeJ2L._SY445_SX342_QL70_FMwebp_.jpg' /></main>
+
     </div>
   )
 }
